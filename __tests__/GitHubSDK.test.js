948f1f8cf546f1398172a42b9dbc22369a33c7ae
Maciej Necka <macieknecka@gmail.com>
2023-11-01 22:06:03 +0100

    Few more tests - Task done

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -32,4 +32,198 @@ describe('GitHubSDK', () => {
     }
     expect(createWrongUserData).toThrow('Owner and SecretToken are required');
   });
+
+  describe('Request Options', () => {
+    it('Should create valid options for GET request', () => {
+      const options = gitHubSDK._createOptions('GET');
+      expect(options.method).toBe('GET');
+      expect(options.headers).toHaveProperty(
+        'Accept',
+        'application/vnd.github.v3+json'
+      );
+      expect(options.headers).toHaveProperty(
+        'Authorization',
+        'token testSecretToken'
+      );
+      expect(options.body).toBeNull();
+    });
+
+    it('Should create valid options for POST request', () => {
+      const data = { key: 'value' };
+      const options = gitHubSDK._createOptions('POST', data);
+      expect(options.method).toBe('POST');
+      expect(options.headers).toHaveProperty(
+        'Accept',
+        'application/vnd.github.v3+json'
+      );
+      expect(options.headers).toHaveProperty(
+        'Authorization',
+        'token testSecretToken'
+      );
+      expect(options.body).toBe(JSON.stringify(data));
+    });
+  });
+
+  describe('Request Errors', () => {
+    it('Should throw an error on failed request', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 403,
+        })
+      );
+      try {
+        await gitHubSDK.getOwner();
+      } catch (error) {
+        expect(error.message).toBe('Request failed with status: 403');
+      }
+    });
+
+    it('Should throw an error on network request failure', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.reject(new Error('Network error'))
+      );
+      try {
+        await gitHubSDK.getOwner();
+      } catch (error) {
+        expect(error.message).toBe('Network error');
+      }
+    });
+
+    it('Should handle unauthorized access with a 401 status code', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 401,
+        })
+      );
+      try {
+        await gitHubSDK.getOwner();
+      } catch (error) {
+        expect(error.message).toBe('Request failed with status: 401');
+      }
+    });
+  });
+
+  describe('Collaborator Invitations', () => {
+    it('Should send a collaborator invitation succesfully', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 204,
+        })
+      );
+      const response = await gitHubSDK.sendInvitation(
+        'testRepo',
+        'testCollaborator'
+      );
+      expect(response.status).toBe(204);
+    });
+
+    it('Should remove a collaborator invitation successfully', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 204,
+        })
+      );
+      const response = await gitHubSDK.removeInvitation(
+        'testRepo',
+        'testInvitationId'
+      );
+      expect(response.status).toBe(204);
+    });
+
+    it('Should handle a successful PUT request to send a collaborator invitation', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 204,
+        })
+      );
+      const originalConsoleLog = console.log;
+      console.log = jest.fn();
+      const response = await gitHubSDK.sendInvitation(
+        'testRepo',
+        'testCollaborator'
+      );
+      expect(response.status).toBe(204);
+      expect(console.log).toHaveBeenCalledWith('Invitation sent successfully');
+      console.log = originalConsoleLog;
+    });
+
+    it('Should handle a successful DELETE request to remove a collaborator invitation', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 204,
+        })
+      );
+
+      const response = await gitHubSDK.removeInvitation(
+        'testRepo',
+        'testInvitationId'
+      );
+      expect(response.status).toBe(204);
+    });
+  });
+
+  describe('Different Scenarios', () => {
+    it('Should handle empty or null owner and secretToken', () => {
+      expect(() => new GitHubSDK('', '')).toThrow(
+        'Owner and SecretToken are required'
+      );
+      expect(() => new GitHubSDK(null, null)).toThrow(
+        'Owner and SecretToken are required'
+      );
+    });
+
+    it('Should handle different authorization tokens', () => {
+      const anotherGitHubSDK = new GitHubSDK('testOwner', 'anotherToken');
+      expect(anotherGitHubSDK.getSecretToken()).toBe('anotherToken');
+    });
+
+    it('Should handle different GitHub API endpoints', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 200,
+          json: () => Promise.resolve({ example: 'response' }),
+        })
+      );
+      const customResponse = await gitHubSDK._fetchData(
+        gitHubSDK._createOptions(),
+        '/custom/endpoint'
+      );
+      expect(customResponse.example).toBe('response');
+    });
+  });
+
+  describe('Data Requests', () => {
+    it('Should handle a succesful GET request for repositories', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 200,
+          json: () => Promise.resolve([{ name: 'Repo1' }, { name: 'Repo2' }]),
+        })
+      );
+      const repos = await gitHubSDK.getRepos();
+      expect(repos).toHaveLength(2);
+      expect(repos[0].name).toBe('Repo1');
+      expect(repos[1].name).toBe('Repo2');
+    });
+
+    it('Should handle a successful GET request for user data', async () => {
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+          status: 200,
+          json: () => Promise.resolve({ name: 'TestUser' }),
+        })
+      );
+      const userData = await gitHubSDK.getOwner();
+      expect(userData.name).toBe('TestUser');
+    });
+  });
 });
