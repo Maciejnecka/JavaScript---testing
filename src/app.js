3fd179f6e7ad1d1bcd783926ac200f855c353ab1
Maciej Necka <macieknecka@gmail.com>
2023-11-01 13:25:49 +0100

    Add some styles and template

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 'use strict';
+import './css/style.css';
 import GitHubSDK from './GitHubSDK.js';
 import { secretToken } from './secretToken.js';
 
-import './css/style.css';
-import { displayProfileData, fetchGitHubData } from './profile.js';
+import {
+  fetchGitHubData,
+  displayProfileData,
+  fetchGitHubRepos,
+  displayReposData,
+} from './profile.js';
 
 document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
   try {
+    updateCopyrightYear();
     const userData = await fetchGitHubData('Maciejnecka', secretToken);
     displayProfileData(userData);
+    const reposData = await fetchGitHubRepos('Maciejnecka', secretToken);
+    displayReposData(reposData);
     const gh = new GitHubSDK('Maciejnecka', secretToken);
-
-    const gh2 = new GitHubSDK('Maciejnecka', secretToken);
-    gh2.sendInvitation('task-js-basics', 'bogolubow');
-    console.log(gh2);
+    gh.sendInvitation('task-js-basics', 'bogolubow');
   } catch (error) {
-    console.error(error);
+    console.error('An error occurred: ', error);
   }
+}
+
+function updateCopyrightYear() {
+  const currentYearElement = document.getElementById('currentYear');
+  const currentYear = new Date().getFullYear();
+  currentYearElement.textContent = currentYear;
 }
--- src/profile.js
+++ src/profile.js
@@ -1,6 +1,8 @@
 'use strict';
 import GitHubSDK from './GitHubSDK';
 
+const MAX_REPOS_TO_SHOW = 9;
+
 export async function fetchGitHubData(username, accessToken) {
   try {
     const gh = new GitHubSDK(username, accessToken);
@@ -12,10 +14,48 @@ export async function fetchGitHubData(username, accessToken) {
 }
 
 export function displayProfileData(data) {
-  const profileElement = document.getElementById('profile');
-  console.log();
+  const user = {
+    avatar: data.avatar_url,
+    name: data.name,
+  };
+  const profileElement = document.querySelector('header');
   profileElement.innerHTML = `
-    <img src="${data.avatar_url}" alt="User Avatar">
-    <p>${data.name}</p>
+  <div class="header__profile-pic-container">
+    <img class="header__profile-pic" src="${user.avatar}" alt="User Avatar">
+    <p class="header__title">${user.name}</p>
+    <p class="header__bio">Wannabe Front-End Developer</p>
+    </div>
+
   `;
+}
+
+export async function fetchGitHubRepos(username, accessToken) {
+  try {
+    const gh = new GitHubSDK(username, accessToken);
+    const reposData = await gh.getRepos();
+    return reposData;
+  } catch (error) {
+    throw new Error(
+      `Error fetching GitHub repositories data: ${error.message}`
+    );
+  }
+}
+
+export function displayReposData(repos) {
+  const reposElement = document.querySelector('.repositories__list');
+  let reposHTML = '';
+
+  for (let i = 0; i < MAX_REPOS_TO_SHOW; i++) {
+    const repo = repos[i];
+    reposHTML += `
+      <li class="repositories__item">
+      <a href="${repo.html_url}" class="repositories__link" target="_blank"
+      > 👉 ${repo.name}</a
+      >
+      <p class="repositories__language">Main language:</p>
+      <p class="repositories__language--display"> ${repo.language}</p>
+      </li>
+      `;
+  }
+  reposElement.innerHTML = reposHTML;
 }
